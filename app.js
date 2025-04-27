7873d804183c2cdd298495efc863991e37191394
littlebabyesma <paksoyesma3461@gmail.com>
2025-04-27 13:43:04 +0300

    static file

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = () => {
-    console.log("Function working!");
-}
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//MIDDLEWARE
+app.use(express.static('public'))
+
+
+app.get('/', (req,res)=>{
+    //dosyayı çözümlemek için
+   res.sendFile(path.resolve(__dirname,"./temp/index.html"));
+});
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Sunucumuz ${port} portunda dinleniyor.`);
+})
